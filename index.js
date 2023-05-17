357582ea5e46b2b70e9b691b1723d5fd7c42d6b5
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-05-17 18:27:43 +0600

    create server site and install important file

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Our toys server is running');
+})
+
+app.listen(port, () => {
+    console.log(`Our toys server running port is ${port}`);
+})
